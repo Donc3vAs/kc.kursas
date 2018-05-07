032e7f9af80e77f08f386ea73502f08785d02b2f
Donc3vas <Donc3vas@NESIOJAMAS-PC>
2018-05-07 21:32:06 +0300

    ....

---
+++ 15Class/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15Class
+{
+    class Auto
+    {
+        public int GimMetai;
+        public string Spalva;
+        public string Marke;
+        public int VIN;
+        public int VNr;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto auto = new Auto();
+            auto.GimMetai = 2010;
+            auto.Spalva = "zalia";
+            Console.WriteLine(auto.Spalva);
+        }
+    }
+}
