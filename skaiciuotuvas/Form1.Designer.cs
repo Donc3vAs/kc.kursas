3b22825ed01b01cce8f5afb318d108a0fac25876
Donc3vas <Donc3vas@NESIOJAMAS-PC>
2018-05-15 22:40:54 +0300

    nauj

---
+++ skaiciuotuvas/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace skaiciuotuvas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.bd = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.be = new System.Windows.Forms.Button();
+            this.b0 = new System.Windows.Forms.Button();
+            this.bda = new System.Windows.Forms.Button();
+            this.bdg = new System.Windows.Forms.Button();
+            this.bm = new System.Windows.Forms.Button();
+            this.bp = new System.Windows.Forms.Button();
+            this.laukas = new System.Windows.Forms.TextBox();
+            this.veiksmas = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(138, 145);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(41, 37);
+            this.b1.TabIndex = 0;
+            this.b1.Text = "1";
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b2
+            // 
+            this.b2.Location = new System.Drawing.Point(186, 145);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(41, 37);
+            this.b2.TabIndex = 1;
+            this.b2.Text = "2";
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.b2_Click);
+            // 
+            // b3
+            // 
+            this.b3.Location = new System.Drawing.Point(233, 145);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(41, 37);
+            this.b3.TabIndex = 2;
+            this.b3.Text = "3";
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.b3_Click);
+            // 
+            // b4
+            // 
+            this.b4.Location = new System.Drawing.Point(138, 188);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(41, 37);
+            this.b4.TabIndex = 3;
+            this.b4.Text = "4";
+            this.b4.UseVisualStyleBackColor = true;
+            this.b4.Click += new System.EventHandler(this.b4_Click);
+            // 
+            // b5
+            // 
+            this.b5.Location = new System.Drawing.Point(186, 188);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(41, 37);
+            this.b5.TabIndex = 4;
+            this.b5.Text = "5";
+            this.b5.UseVisualStyleBackColor = true;
+            this.b5.Click += new System.EventHandler(this.b5_Click);
+            // 
+            // b6
+            // 
+            this.b6.Location = new System.Drawing.Point(233, 188);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(41, 37);
+            this.b6.TabIndex = 5;
+            this.b6.Text = "6";
+            this.b6.UseVisualStyleBackColor = true;
+            this.b6.Click += new System.EventHandler(this.b6_Click);
+            // 
+            // b8
+            // 
+            this.b8.Location = new System.Drawing.Point(186, 231);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(41, 37);
+            this.b8.TabIndex = 6;
+            this.b8.Text = "8";
+            this.b8.UseVisualStyleBackColor = true;
+            this.b8.Click += new System.EventHandler(this.b8_Click);
+            // 
+            // b7
+            // 
+            this.b7.Location = new System.Drawing.Point(139, 231);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(41, 37);
+            this.b7.TabIndex = 7;
+            this.b7.Text = "7";
+            this.b7.UseVisualStyleBackColor = true;
+            this.b7.Click += new System.EventHandler(this.b7_Click);
+            // 
+            // bd
+            // 
+            this.bd.Location = new System.Drawing.Point(186, 274);
+            this.bd.Name = "bd";
+            this.bd.Size = new System.Drawing.Size(41, 37);
+            this.bd.TabIndex = 8;
+            this.bd.Text = ",";
+            this.bd.UseVisualStyleBackColor = true;
+            this.bd.Click += new System.EventHandler(this.bd_Click);
+            // 
+            // b9
+            // 
+            this.b9.Location = new System.Drawing.Point(233, 231);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(41, 37);
+            this.b9.TabIndex = 9;
+            this.b9.Text = "9";
+            this.b9.UseVisualStyleBackColor = true;
+            this.b9.Click += new System.EventHandler(this.b9_Click);
+            // 
+            // be
+            // 
+            this.be.Location = new System.Drawing.Point(233, 274);
+            this.be.Name = "be";
+            this.be.Size = new System.Drawing.Size(41, 37);
+            this.be.TabIndex = 10;
+            this.be.Text = "=";
+            this.be.UseVisualStyleBackColor = true;
+            this.be.Click += new System.EventHandler(this.be_Click);
+            // 
+            // b0
+            // 
+            this.b0.Location = new System.Drawing.Point(138, 274);
+            this.b0.Name = "b0";
+            this.b0.Size = new System.Drawing.Size(41, 37);
+            this.b0.TabIndex = 11;
+            this.b0.Text = "0";
+            this.b0.UseVisualStyleBackColor = true;
+            this.b0.Click += new System.EventHandler(this.b0_Click);
+            // 
+            // bda
+            // 
+            this.bda.Location = new System.Drawing.Point(280, 145);
+            this.bda.Name = "bda";
+            this.bda.Size = new System.Drawing.Size(41, 37);
+            this.bda.TabIndex = 12;
+            this.bda.Text = "/";
+            this.bda.UseVisualStyleBackColor = true;
+            this.bda.Click += new System.EventHandler(this.bda_Click);
+            // 
+            // bdg
+            // 
+            this.bdg.Location = new System.Drawing.Point(280, 188);
+            this.bdg.Name = "bdg";
+            this.bdg.Size = new System.Drawing.Size(41, 37);
+            this.bdg.TabIndex = 13;
+            this.bdg.Text = "*";
+            this.bdg.UseVisualStyleBackColor = true;
+            this.bdg.Click += new System.EventHandler(this.bdg_Click);
+            // 
+            // bm
+            // 
+            this.bm.Location = new System.Drawing.Point(280, 231);
+            this.bm.Name = "bm";
+            this.bm.Size = new System.Drawing.Size(41, 37);
+            this.bm.TabIndex = 14;
+            this.bm.Text = "-";
+            this.bm.UseVisualStyleBackColor = true;
+            this.bm.Click += new System.EventHandler(this.bm_Click);
+            // 
+            // bp
+            // 
+            this.bp.Location = new System.Drawing.Point(280, 274);
+            this.bp.Name = "bp";
+            this.bp.Size = new System.Drawing.Size(41, 37);
+            this.bp.TabIndex = 15;
+            this.bp.Text = "+";
+            this.bp.UseVisualStyleBackColor = true;
+            this.bp.Click += new System.EventHandler(this.bp_Click);
+            // 
+            // laukas
+            // 
+            this.laukas.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.laukas.Location = new System.Drawing.Point(13, 34);
+            this.laukas.Multiline = true;
+            this.laukas.Name = "laukas";
+            this.laukas.ReadOnly = true;
+            this.laukas.Size = new System.Drawing.Size(333, 40);
+            this.laukas.TabIndex = 16;
+            this.laukas.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // veiksmas
+            // 
+            this.veiksmas.Location = new System.Drawing.Point(12, 90);
+            this.veiksmas.Name = "veiksmas";
+            this.veiksmas.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.veiksmas.Size = new System.Drawing.Size(334, 20);
+            this.veiksmas.TabIndex = 17;
+            this.veiksmas.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 342);
+            this.Controls.Add(this.veiksmas);
+            this.Controls.Add(this.laukas);
+            this.Controls.Add(this.bp);
+            this.Controls.Add(this.bm);
+            this.Controls.Add(this.bdg);
+            this.Controls.Add(this.bda);
+            this.Controls.Add(this.b0);
+            this.Controls.Add(this.be);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.bd);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button bd;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button be;
+        private System.Windows.Forms.Button b0;
+        private System.Windows.Forms.Button bda;
+        private System.Windows.Forms.Button bdg;
+        private System.Windows.Forms.Button bm;
+        private System.Windows.Forms.Button bp;
+        private System.Windows.Forms.Label veiksmas;
+        private System.Windows.Forms.TextBox laukas;
+    }
+}
+
---
+++ skaiciuotuvas/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace skaiciuotuvas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Button mygtukasn = new Button();
+        Button mygtukasz = new Button();
+        Double sk1 = 0;
+        Double sk2 = 0;
+        bool k = false;
+        bool j = false;
+        private void b1_Click(object sender, EventArgs e)
+        {
+            mygtukasn = b1;
+            sk(b1);
+        }
+
+        private void b2_Click(object sender, EventArgs e)
+        {
+            mygtukasn = b2;
+            sk(mygtukasn);
+        }
+
+        private void b3_Click(object sender, EventArgs e)
+        {
+            mygtukasn = b3;
+            sk(mygtukasn);
+        }
+
+        private void b4_Click(object sender, EventArgs e)
+        {
+            mygtukasn = b4;
+            sk(mygtukasn);
+        }
+
+        private void b5_Click(object sender, EventArgs e)
+        {
+            mygtukasn = b5;
+            sk(mygtukasn);
+        }
+
+        private void b6_Click(object sender, EventArgs e)
+        {
+            mygtukasn = b6;
+            sk(mygtukasn);
+        }
+
+        private void b7_Click(object sender, EventArgs e)
+        {
+            mygtukasn = b7;
+            sk(mygtukasn);
+        }
+
+        private void b8_Click(object sender, EventArgs e)
+        {
+            mygtukasn = b8;
+            sk(mygtukasn);
+        }
+
+        private void b9_Click(object sender, EventArgs e)
+        {
+            mygtukasn = b9;
+            sk(mygtukasn);
+        }
+
+        private void b0_Click(object sender, EventArgs e)
+        {            
+                mygtukasn = b0;
+                sk(mygtukasn);                        
+        }
+
+        private void bd_Click(object sender, EventArgs e)
+        {
+            mygtukasz = bd;
+            zn(mygtukasz);
+        }
+
+        private void bp_Click(object sender, EventArgs e)
+        {
+            mygtukasz = bp;
+            zn(mygtukasz);
+        }
+
+        private void bm_Click(object sender, EventArgs e)
+        {
+            mygtukasz = bm;
+            zn(mygtukasz);
+        }
+
+        private void bdg_Click(object sender, EventArgs e)
+        {
+            mygtukasz = bdg;
+            zn(mygtukasz);
+        }
+
+        private void bda_Click(object sender, EventArgs e)
+        {
+            mygtukasz = bda;
+            zn(mygtukasz);
+        }
+
+        private void be_Click(object sender, EventArgs e)
+        {
+            mygtukasz = be;
+            zn(mygtukasz);
+        }
+
+
+        // skaiciai
+        private void sk(Button x)
+        {
+            if (k)
+            {
+                laukas.Text = null;
+                k = false;
+            }
+            if (x.Text == "0" & laukas.Text == "")
+            {
+                laukas.Text = "0,";
+                veiksmas.Text = "0,";
+            }
+            else
+            {
+                laukas.Text += x.Text;
+                veiksmas.Text += x.Text;
+            }            
+        }
+
+        // zenklai
+        private void zn (Button x)
+        {
+            if (x.Text != "," & x.Text != "=")
+            {
+                if (laukas.Text == veiksmas.Text)
+                {
+                    veiksmas.Text += x.Text;
+                    sk1 = Convert.ToDouble(laukas.Text);
+                    laukas.Text = null;
+                }
+                else
+                {
+                    sk2 = Convert.ToDouble(laukas.Text);
+                    sk1 = vk(sk1, sk2, veiksmas.Text[veiksmas.Text.Length - laukas.Text.Length - 1]);
+                    laukas.Text = Convert.ToString(sk1);
+                    veiksmas.Text += x.Text;
+                    k = true;
+                }
+            }            
+            else if (x.Text == ",")
+            {
+                if (x.Text == "," & laukas.Text == "")
+                {
+                    laukas.Text = "0,";
+                    veiksmas.Text = "0,";
+                }
+                else
+                {
+                    foreach (var n in laukas.Text)
+                    {
+                        if (n == ',')
+                        {
+                            j = true;
+                        }
+                    }
+                    if (!j)
+                    {
+                        veiksmas.Text += x.Text;
+                        laukas.Text += x.Text;
+                        j = false;
+                    }
+                }
+            }
+            else if (x.Text == "=")
+            {
+                sk2 = Convert.ToDouble(laukas.Text);
+                sk1 = vk(sk1, sk2, veiksmas.Text[veiksmas.Text.Length - laukas.Text.Length - 1]);
+                laukas.Text = Convert.ToString(sk1);
+                veiksmas.Text += x.Text + laukas.Text;
+            }
+        }
+        private Double vk(double x, double y, char z)
+        {
+            if (z == '+') { x += y; }
+            else if (z == '-') { x -= y; }
+            else if (z == '*') { x *= y; }
+            else if (z == '/') { x /= y; }
+            return x;
+        }
+    }
+}
